0dc55e3d9fe9d65071e4929781dd3a6fec6df3e5
Bartłomiej Kukuczka <kukuczkabartlomiej@gmail.com>
2024-03-22 12:47:08 +0100

    init repository

---
+++ src/main/java/com/example/kafkaedu/KafkaEduApplication.java
@@ -0,0 +1,13 @@
+package com.example.kafkaedu;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KafkaEduApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KafkaEduApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/kafkaedu/KafkaEduApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.kafkaedu;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KafkaEduApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
